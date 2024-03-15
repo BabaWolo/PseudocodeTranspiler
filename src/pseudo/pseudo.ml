6a0e7124e1d4a2eb3210f473f36e553e46885c4a
LucaPurcilly <lp@enablment.com>
2024-03-15 11:39:00 +0100

    Adds ability to print tokens

--- src/pseudo/pseudo.ml
+++ src/pseudo/pseudo.ml
@@ -1,9 +1,27 @@
 open Ast
 
+(* 
+(* Print tokens *)
+let print_token = function
+  | Parser.EOF -> print_endline "EOF"
+  | Parser.PLUS -> print_endline "PLUS"
+  | Parser.INT x -> print_endline ("INT " ^ string_of_int x)
+
+let () =
+  let lexbuf = Lexing.from_string "3+4" in
+  let rec print_tokens () =
+    let token = Lexer.token lexbuf in
+    print_token token;
+    match token with
+    | Parser.EOF -> ()
+    | _ -> print_tokens ()
+  in
+  print_tokens () *)
 
 let () =
   let lexbuf = Lexing.from_string "3+4" in
   let ast = Parser.program Lexer.token lexbuf in
+
   match ast with
   | Cexpr(Ebinop((Badd, Ecst(Cint(x)), Ecst(Cint(y)))))  ->
     let result = x + y in
